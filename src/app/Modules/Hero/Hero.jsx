a12af8b77b8bd5d470d1ff22eacb4a7810d93f96
Pheonix999777 <Pheonix4777@gmail.com>
2024-07-25 13:51:15 +0500

    change Hero

--- src/app/Modules/Hero/Hero.jsx
+++ src/app/Modules/Hero/Hero.jsx
@@ -6,28 +6,30 @@ export default function Hero() {
   return (
     <section className="hero">
       <Container>
-        <h1 className="hero__titile">
-          Если вы не пользуетесь своими правами, то вашими правами будут
-          пользоваться <span style={{ color: "#026D2A" }}>другие</span>
-        </h1>
-        <div className="hero__flex">
-          <div className="hero__box">
-            <span className="hero__text">Для юридических лиц</span>
-            <a className="hero__link" href="">
-              Подробнее
-              <span style={{ marginLeft: "10px" }}>
-                <ArrowRight />
-              </span>
-            </a>
-          </div>
-          <div className="hero__box">
-            <span className="hero__text">Для юридических лиц</span>
-            <a className="hero__link" href="">
-              Подробнее
-              <span style={{ marginLeft: "10px" }}>
-                <ArrowRight />
-              </span>
-            </a>
+        <div className="hero__test">
+          <h1 className="hero__titile">
+            Если вы не пользуетесь своими правами, то вашими правами будут
+            пользоваться <span style={{ color: "#026D2A" }}>другие</span>
+          </h1>
+          <div className="hero__flex">
+            <div className="hero__box">
+              <span className="hero__text">Для юридических лиц</span>
+              <a className="hero__link" href="">
+                Подробнее
+                <span style={{ marginLeft: "10px" }}>
+                  <ArrowRight />
+                </span>
+              </a>
+            </div>
+            <div className="hero__box">
+              <span className="hero__text">Для юридических лиц</span>
+              <a className="hero__link" href="">
+                Подробнее
+                <span style={{ marginLeft: "10px" }}>
+                  <ArrowRight />
+                </span>
+              </a>
+            </div>
           </div>
         </div>
       </Container>
