c2ac67761f588f7b35ae4c17aa36e46502ae1991
Pheonix999777 <Pheonix4777@gmail.com>
2024-07-30 15:27:44 +0500

    dadasd

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,10 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* <link
+        <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap"
-        /> */}
+        />
       </head>
       <body>
         <Header />
